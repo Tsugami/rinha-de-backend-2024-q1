42c5c9fa85f5c8b9091ac8c5a6b866d7266d1329
Yslan Ramos <yslanramos.r@gmail.com>
2024-02-06 00:21:07 -0300

    returns 404 when account not found

--- src/transactions/create-transaction.repository.ts
+++ src/transactions/create-transaction.repository.ts
@@ -4,57 +4,54 @@ import type {
   CreateTransactionInputDto,
   CreateTransactionOutputDto,
 } from "./dto/create-transaction.dto";
+import { Prisma } from "@prisma/client";
 
 export const createTransaction = async (
   input: CreateTransactionInputDto
 ): Promise<CreateTransactionOutputDto> => {
-  const account = await db.account.findUnique({
-    where: { id: input.cliente_id },
-  });
-
-  if (!account) {
-    throw new NotFoundError("Account not found");
-  }
-
-  // TODO check by database before
-  // if (input.tipo === "d" && input.valor > account.saldo + account.limite) {
-  //   throw new NOT("Insufficient funds");
-  // }
-
   return db.$transaction(async (tx) => {
     const isDebit = input.tipo === "d";
     // const isOverdraft = isDebit && input.valor > account.saldo + account.limite;
-
-    const sender = await tx.account.update({
-      data: {
-        [isDebit ? "limite" : "saldo"]: {
-          increment: input.valor,
-        },
-      },
-      where: {
-        id: input.cliente_id,
-      },
-    });
-
-    const result = await tx.transaction.create({
-      data: {
-        account_id: sender.id,
-        valor: input.valor,
-        tipo: input.tipo,
-      },
-      select: {
-        account: {
-          select: {
-            limite: true,
-            saldo: true,
+    try {
+      const sender = await tx.account.update({
+        data: {
+          [isDebit ? "limite" : "saldo"]: {
+            increment: input.valor,
           },
         },
-      },
-    });
+        where: {
+          id: input.cliente_id,
+        },
+      });
 
-    return {
-      limite: Number(result.account.limite),
-      saldo: Number(result.account.saldo),
-    };
+      const result = await tx.transaction.create({
+        data: {
+          account_id: sender.id,
+          valor: input.valor,
+          tipo: input.tipo,
+        },
+        select: {
+          account: {
+            select: {
+              limite: true,
+              saldo: true,
+            },
+          },
+        },
+      });
+
+      return {
+        limite: Number(result.account.limite),
+        saldo: Number(result.account.saldo),
+      };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2025") {
+          throw new NotFoundError("Account not found");
+        }
+        // console.log(error.code + );
+      }
+      throw error;
+    }
   });
 };
